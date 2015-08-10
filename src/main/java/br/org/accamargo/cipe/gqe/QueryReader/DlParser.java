4d6584c2b689245bb07e6672b7b8413a4546055c
Diogo Patrao <djogopatrao@gmail.com>
2015-08-10 18:21:45 -0300

    #10 fixing bug in prototype class

--- src/main/java/br/org/accamargo/cipe/gqe/QueryReader/DlParser.java
+++ src/main/java/br/org/accamargo/cipe/gqe/QueryReader/DlParser.java
@@ -1,11 +1,21 @@
 package br.org.accamargo.cipe.gqe.QueryReader;
 
+import com.hp.hpl.jena.ontology.OntModel;
+
 public class DlParser implements BaseParser {
 
 	@Override
-	public ParsedQuery parse(String query) throws Exception {
+	public ParsedQuery parse(String query, OntModel model) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public ParsedQuery parse(String query, OntModel model, String defaultUri)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
